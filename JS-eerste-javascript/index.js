d6181e3531a6bb1096f2f8e35fea50e843cbdc66
JEBesteman <86465468+JEBesteman@users.noreply.github.com>
2021-07-28 09:34:47 +0200

    Add files via upload

---
+++ JS-eerste-javascript/index.js
@@ -0,0 +1,36 @@
+console.log("Hello Winc Academy");
+
+let name = "Jolanda";
+console.log(name);
+
+//let if = "winner";
+//kan niet omdat if een reserved keyword is
+
+let additionNumbers = 4 + 4;
+console.log(additionNumbers); //8
+
+let additionStrings = "4" + "4";
+console.log(additionStrings); //44
+
+//numbers worden echt opgeteld, strings worden aan elkaar toegevoegd
+
+let addition = 100 + 2 + 36;
+console.log(addition); 
+
+let subtraction = 1000 - 7 - 13 - 5;
+console.log(subtraction); 
+
+let multiplication = 25 * 5 * 9;
+console.log(multiplication);
+
+let division = 125 / 25;
+console.log(division);
+
+let modulus = 125 % 4;
+console.log(modulus);
+
+let age = 43;
+console.log(typeof age); //number
+
+let leeftijd = "43";
+console.log(typeof leeftijd); //string
